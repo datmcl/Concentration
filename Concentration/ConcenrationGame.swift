da504a39c04dc6a3f63576f9707bb2847cd7d8bf
datmcl <mixail.bokov9@gmail.com>
2021-05-10 23:26:30 +0700

    add filtering and extension

--- Concentration/ConcenrationGame.swift
+++ Concentration/ConcenrationGame.swift
@@ -12,17 +12,7 @@ struct ConcentrationGame {
     
     private var indexOneAndOnlyFaceUp: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
         }
         set {
             for index in cards.indices {
@@ -54,4 +44,10 @@ struct ConcentrationGame {
         cards.shuffle()
     }
     
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
 }
